dfda523d35112320c2da99bdfa6784f578c1fe8f
Alexander Novikov <mem-memov@yandex.ru>
2019-12-29 00:50:09 +0300

    body head

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -21,35 +21,41 @@ import           Data.Monoid ((<>))
 
 frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
-  { _frontend_head = do
-      el "title" $ text "Obelisk Minimal Example"
-      elAttr "meta" ("charset" =: "utf-8") blank
-      elAttr "meta" (Map.fromList [("name", "viewport"), ("content", "width=device-width, initial-scale=1")]) blank
-      elAttr "link" (("rel" =: "stylesheet") <> ("href" =: "https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css")) blank
-      elAttr "script" (("defer" =: "defer") <> ("src" =: "https://use.fontawesome.com/releases/v5.3.1/js/all.js")) $ text ""
-      return ()
-  , _frontend_body = do
+  { _frontend_head = prerender_ (text "Loading..") headElement
+  , _frontend_body = prerender_ (text "Loading...") bodyElement
+  }
+
+headElement :: MonadWidget t m => m ()
+headElement = do
+  el "title" $ text "Obelisk Minimal Example"
+  elAttr "meta" ("charset" =: "utf-8") blank
+  elAttr "meta" (Map.fromList [("name", "viewport"), ("content", "width=device-width, initial-scale=1")]) blank
+  elAttr "link" (("rel" =: "stylesheet") <> ("href" =: "https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css")) blank
+  elAttr "script" (("defer" =: "defer") <> ("src" =: "https://use.fontawesome.com/releases/v5.3.1/js/all.js")) $ text ""
+  return ()
+  
+bodyElement :: MonadWidget t m => m ()
+bodyElement = do
     elClass "section" "hero is-fullheight" $ do
       elClass "div" "hero-head" $ blank
       elClass "div" "hero-body" $ do
         elAttr "div" (Map.fromList [("style", "display: flex; flex-grow: 1; flex-shrink: 1; align-self: stretch; position: relative")]) $ do
-          -- elAttr "div" (Map.fromList [("style", "position: absolute; width: 100%; height: 100%; display: flex; flex-direction: column; overflow-y: auto;")]) $ do
-          elAttr "div" (Map.fromList [("style", "position: absolute; width: 100%; height: 100%; display: flex; flex-direction: column; flex-wrap: wrap-reverse; align-items: flex-end; justify-content: flex-end;")]) $ do
-            elClass "span" "tag is-primary" $ text "1"
-            elClass "div" "tag is-primary" $ text "2"
-            elClass "div" "tag is-primary" $ text "3"
-            elClass "div" "tag is-primary" $ text "4"
-            elClass "div" "tag is-primary" $ text "5"
-            elClass "div" "tag is-primary" $ text "6"
-            elClass "div" "tag is-primary" $ text "7"
-            elClass "div" "tag is-primary" $ text "8"
-            elClass "div" "tag is-primary" $ text "9"
-            elClass "div" "tag is-primary" $ text "10"
-            elClass "div" "tag is-primary" $ text "11"
-            elClass "div" "tag is-primary" $ text "12"
-            elClass "div" "tag is-primary" $ text "13"
-            elClass "div" "tag is-primary" $ text "14"
-            elClass "div" "tag is-primary" $ text "15"
+          elAttr "div" (Map.fromList [("style", "position: absolute; width: 100%; height: 100%; overflow-y: auto;")]) $ do
+            elClass "div" "" $ text "1"
+            elClass "div" "" $ text "2"
+            elClass "div" "" $ text "3"
+            elClass "div" "" $ text "4"
+            elClass "div" "" $ text "5"
+            elClass "div" "" $ text "6"
+            elClass "div" "" $ text "7"
+            elClass "div" "" $ text "8"
+            elClass "div" "" $ text "9"
+            elClass "div" "" $ text "10"
+            elClass "div" "" $ text "11"
+            elClass "div" "" $ text "12"
+            elClass "div" "" $ text "13"
+            elClass "div" "" $ text "14"
+            elClass "div" "" $ text "15"
       elClass "div" "hero-foot" $ do
         elClass "div" "container" $ do
           elAttr "input" (Map.fromList [("type", "text"), ("class", "input")]) $ blank
@@ -66,4 +72,3 @@ frontend = Frontend
             elClass "span" "icon is-small" $ do
               elClass "i" "fas fa-redo" $ blank
     return ()
-  }
