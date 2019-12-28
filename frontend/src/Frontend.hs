d7b7d450f5ce673fb4cec3c419517f64ffa67609
Alexander Novikov <mem-memov@yandex.ru>
2019-12-29 01:01:30 +0300

    splitting into parts

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -40,35 +40,43 @@ bodyElement = do
       elClass "div" "hero-head" $ blank
       elClass "div" "hero-body" $ do
         elAttr "div" (Map.fromList [("style", "display: flex; flex-grow: 1; flex-shrink: 1; align-self: stretch; position: relative")]) $ do
-          elAttr "div" (Map.fromList [("style", "position: absolute; width: 100%; height: 100%; overflow-y: auto;")]) $ do
-            elClass "div" "" $ text "1"
-            elClass "div" "" $ text "2"
-            elClass "div" "" $ text "3"
-            elClass "div" "" $ text "4"
-            elClass "div" "" $ text "5"
-            elClass "div" "" $ text "6"
-            elClass "div" "" $ text "7"
-            elClass "div" "" $ text "8"
-            elClass "div" "" $ text "9"
-            elClass "div" "" $ text "10"
-            elClass "div" "" $ text "11"
-            elClass "div" "" $ text "12"
-            elClass "div" "" $ text "13"
-            elClass "div" "" $ text "14"
-            elClass "div" "" $ text "15"
-      elClass "div" "hero-foot" $ do
-        elClass "div" "container" $ do
-          elAttr "input" (Map.fromList [("type", "text"), ("class", "input")]) $ blank
-          elClass "button" "button is-rounded" $ do
-            elClass "span" "icon is-small" $ do
-              elClass "i" "fas fa-search" $ blank
-          elClass "button" "button is-rounded" $ do
-            elClass "span" "icon is-small" $ do
-              elClass "i" "fas fa-backspace" $ blank
-          elClass "button" "button is-rounded" $ do
-            elClass "span" "icon is-small" $ do
-              elClass "i" "fas fa-undo" $ blank
-          elClass "button" "button is-rounded" $ do
-            elClass "span" "icon is-small" $ do
-              elClass "i" "fas fa-redo" $ blank
-    return ()
+          elAttr "div" (Map.fromList [("style", "position: absolute; width: 100%; height: 100%;")]) $ do
+            messageListElement
+      elClass "div" "hero-foot" $ toolbarElement
+
+messageListElement :: MonadWidget t m => m ()
+messageListElement = do
+  elAttr "div" (Map.fromList [("style", "width: 100%; height: 100%; overflow-y: auto;")]) $ do
+    elClass "div" "" $ text "1"
+    elClass "div" "" $ text "2"
+    elClass "div" "" $ text "3"
+    elClass "div" "" $ text "4"
+    elClass "div" "" $ text "5"
+    elClass "div" "" $ text "6"
+    elClass "div" "" $ text "7"
+    elClass "div" "" $ text "8"
+    elClass "div" "" $ text "9"
+    elClass "div" "" $ text "10"
+    elClass "div" "" $ text "11"
+    elClass "div" "" $ text "12"
+    elClass "div" "" $ text "13"
+    elClass "div" "" $ text "14"
+    elClass "div" "" $ text "15"
+
+toolbarElement :: MonadWidget t m => m ()
+toolbarElement = do
+  elClass "div" "container" $ do
+    elAttr "input" (Map.fromList [("type", "text"), ("class", "input")]) $ blank
+    elClass "button" "button is-rounded" $ do
+      elClass "span" "icon is-small" $ do
+        elClass "i" "fas fa-search" $ blank
+    elClass "button" "button is-rounded" $ do
+      elClass "span" "icon is-small" $ do
+        elClass "i" "fas fa-backspace" $ blank
+    elClass "button" "button is-rounded" $ do
+      elClass "span" "icon is-small" $ do
+        elClass "i" "fas fa-undo" $ blank
+    elClass "button" "button is-rounded" $ do
+      elClass "span" "icon is-small" $ do
+        elClass "i" "fas fa-redo" $ blank
+  return ()
