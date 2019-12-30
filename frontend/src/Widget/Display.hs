dac331f78db4c64c8e0f2e94bb98fa2829080af3
Alexander Novikov <aleksandr.novikov@homeapp.ru>
2019-12-30 17:05:43 +0300

    dispaly widget type

--- frontend/src/Widget/Display.hs
+++ frontend/src/Widget/Display.hs
@@ -6,13 +6,13 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Widget.Display (widget) where
 
-
-import           Control.Lens
-import qualified Data.Map     as M
 import qualified Data.Text    as T
 import           Reflex.Dom
 import Control.Monad.Fix (MonadFix)
 
+widget :: (DomBuilder t m, PostBuild t m, MonadHold t m) 
+    => Event t T.Text
+    -> m ()
 widget textEvent = do
   textDynamic <- holdDyn "" textEvent 
   elClass "div" "box" $ dynText textDynamic
