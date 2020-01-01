7699b65a6672af62a133be9b25046e47c3007fe7
Alexander Novikov <mem-memov@yandex.ru>
2020-01-01 14:34:07 +0300

    filter all

--- frontend/src/MessageList.hs
+++ frontend/src/MessageList.hs
@@ -16,10 +16,14 @@ data Input t = Input {
 widget :: MonadWidget t m => Input t -> m ()
 widget input = do
   elAttr "div" (Map.fromList [("style", "width: 100%; height: 100%; overflow-y: auto;")]) $ do
-    _ <- simpleList (inputMessages input) displaySomeData
+    _ <- simpleList (filterMessages input) displaySomeData
     return ()
 
 displaySomeData :: MonadWidget t m => Dynamic t T.Text -> m ()
 displaySomeData dynamicMessage = do
-  elClass "div" "" $ dynText $ dynamicMessage
+  elClass "div" "" $ do
+    elClass "span" "tag is-primary" $ dynText $ dynamicMessage
   return ()
+
+filterMessages :: Input t -> Dynamic t [T.Text]
+filterMessages input = inputMessages input
